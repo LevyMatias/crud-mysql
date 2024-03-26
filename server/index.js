c8f170d5fecfeaa56753c8b5a5467261aa2d3556
Levy <levisilvas2018@gmail.com>
2024-03-26 15:57:10 -0300

    vercel config

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import userRoutes from './routes/users.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors())
